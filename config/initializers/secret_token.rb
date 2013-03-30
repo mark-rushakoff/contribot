de8f1dc2e079b116d7740f9e8e275cecbab8699f
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-03-30 12:21:13 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contribot::Application.config.secret_token = 'b506926369ab2fb983aed98cef7d7193b004f373887835ee9d8966572b4e212443e595c019ff995a0a7149670be47f0116af61fa197f189ed21a4aeae25fb55c'
