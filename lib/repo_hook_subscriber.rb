8ca14d60bba3b46bab26ef78a30563169a7dc8f5
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-04-05 22:19:02 -0700

    RepoHookSubscriber uses OAuth2 token

--- lib/repo_hook_subscriber.rb
+++ lib/repo_hook_subscriber.rb
@@ -1,13 +1,19 @@
 module RepoHookSubscriber
   class << self
     def subscribe(watched_repo, callback_url)
-      url = "https://api.github.com/hub?client_id=#{ENV['CONTRIBOT_GITHUB_CLIENT_ID']}&client_secret=#{ENV['CONTRIBOT_GITHUB_CLIENT_SECRET']}"
-      HTTParty.post(url, body: {
+      # Authenticate via OAuth2 token: http://developer.github.com/v3/#authentication
+      url = "https://api.github.com/hub?access_token=#{ENV['CONTRIBOT_GITHUB_OAUTH2_TOKEN']}"
+      resp = HTTParty.post(url, body: {
         'hub.mode' => 'subscribe',
         'hub.topic' => "https://github.com/#{watched_repo.owner}/#{watched_repo.repo_name}/events/#{watched_repo.event}",
         'hub.callback' => callback_url,
         'hub.secret' => watched_repo.secret
-      }).success?
+      })
+
+      return true if resp.success?
+
+      $stderr.puts("Subscription failure: #{resp.inspect}")
+      false
     end
   end
 end
--- spec/repo_hook_subscriber_spec.rb
+++ spec/repo_hook_subscriber_spec.rb
@@ -4,11 +4,10 @@ describe RepoHookSubscriber do
   describe '.subscribe' do
     it 'makes the right request' do
       repo = double(owner: 'octocat', repo_name: 'foobar', event: 'pull_request', secret: 'a_hex_string')
-      ENV.stub(:[]).with('CONTRIBOT_GITHUB_CLIENT_ID').and_return('gh_id')
-      ENV.stub(:[]).with('CONTRIBOT_GITHUB_CLIENT_SECRET').and_return('gh_secret')
+      ENV.stub(:[]).with('CONTRIBOT_GITHUB_OAUTH2_TOKEN').and_return('a_token')
 
       callback_url = 'http://example.com/callback'
-      stubbed_request = stub_request(:post, "https://api.github.com/hub?client_id=gh_id&client_secret=gh_secret").
+      stubbed_request = stub_request(:post, "https://api.github.com/hub?access_token=a_token").
          with(:body => "hub.mode=subscribe&hub.topic=https%3A%2F%2Fgithub.com%2Foctocat%2Ffoobar%2Fevents%2Fpull_request&hub.callback=http%3A%2F%2Fexample.com%2Fcallback&hub.secret=a_hex_string").
          to_return(:status => 200, :body => "", :headers => {})
 
@@ -18,12 +17,12 @@ describe RepoHookSubscriber do
     end
 
     it 'returns false on a failed request' do
+      $stderr.stub(:puts) # suppress error output for this spec
       repo = double(owner: 'octocat', repo_name: 'foobar', event: 'pull_request', secret: 'a_hex_string')
-      ENV.stub(:[]).with('CONTRIBOT_GITHUB_CLIENT_ID').and_return('gh_id')
-      ENV.stub(:[]).with('CONTRIBOT_GITHUB_CLIENT_SECRET').and_return('gh_secret')
+      ENV.stub(:[]).with('CONTRIBOT_GITHUB_OAUTH2_TOKEN').and_return('a_token')
 
       callback_url = 'http://example.com/callback'
-      stubbed_request = stub_request(:post, "https://api.github.com/hub?client_id=gh_id&client_secret=gh_secret").
+      stubbed_request = stub_request(:post, "https://api.github.com/hub?access_token=a_token").
          with(:body => "hub.mode=subscribe&hub.topic=https%3A%2F%2Fgithub.com%2Foctocat%2Ffoobar%2Fevents%2Fpull_request&hub.callback=http%3A%2F%2Fexample.com%2Fcallback&hub.secret=a_hex_string").
          to_return(:status => 500, :body => "", :headers => {})
 
