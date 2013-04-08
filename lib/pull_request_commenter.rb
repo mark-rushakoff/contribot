5a96cf5bceda100db9e3850d2a68138cf8436e32
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-04-08 08:05:50 -0700

    Log when commenting on pull request

--- lib/pull_request_commenter.rb
+++ lib/pull_request_commenter.rb
@@ -2,14 +2,22 @@ module PullRequestCommenter
   class << self
     def comment_approved(opts)
       client = Octokit::Client.new(login: ENV['CONTRIBOT_BOT_NAME'], oauth_token: ENV['CONTRIBOT_BOT_TOKEN'])
-      message = "Thanks for signing the CLA, @#{opts[:pull_requester]}! This pull request is ready for review."
-      client.add_comment(opts[:repo], opts[:issue_id], message)
+      pull_requester = opts[:pull_requester]
+      repo = opts[:repo]
+      issue_id = opts[:issue_id]
+      message = "Thanks for signing the CLA, @#{pull_requester}! This pull request is ready for review."
+      client.add_comment(repo, issue_id, message)
+      Rails.logger.info("Commented on pull request from approved user #{pull_requester} on #{repo} ##{issue_id}")
     end
 
     def comment_needs_approval(opts)
       client = Octokit::Client.new(login: ENV['CONTRIBOT_BOT_NAME'], oauth_token: ENV['CONTRIBOT_BOT_TOKEN'])
-      message = "Thank you for the pull request, @#{opts[:pull_requester]}. It looks like you haven't signed our CLA yet."
-      client.add_comment(opts[:repo], opts[:issue_id], message)
+      pull_requester = opts[:pull_requester]
+      repo = opts[:repo]
+      issue_id = opts[:issue_id]
+      message = "Thank you for the pull request, @#{pull_requester}. It looks like you haven't signed our CLA yet."
+      client.add_comment(repo, issue_id, message)
+      Rails.logger.info("Commented on pull request from unapproved user #{pull_requester} on #{repo} ##{issue_id}")
     end
   end
 end
