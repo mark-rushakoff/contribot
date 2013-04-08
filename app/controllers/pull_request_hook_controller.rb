f44163ecf5cf9f3938f663bcfcf57886fd5f00c9
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-04-07 23:36:52 -0700

    Temporarily check signatures from GitHub

--- app/controllers/pull_request_hook_controller.rb
+++ app/controllers/pull_request_hook_controller.rb
@@ -1,5 +1,12 @@
 class PullRequestHookController < ApplicationController
   def create
+    watched_repo = WatchedRepo.find_by_owner_and_repo_name(params[:owner], params[:repo_name])
+    render nothing: true and return if watched_repo.nil?
+    calculated_sha1 = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), watched_repo.secret, request.raw_post)
+    Rails.logger.info("Repo secret: #{watched_repo.secret}")
+    Rails.logger.info("Calculated sha1: #{calculated_sha1}")
+    Rails.logger.info("Received sha1: #{request.headers['X-Hub-Signature']}")
+
     payload = JSON.parse(params[:payload])
 
     render nothing: true and return unless payload['action'] == 'opened'
